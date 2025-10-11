9049e3c43562df6b3b9d36e396083b385dcb351c
jdsatsatin <dev.jdsatsatin@gmail.com>
2025-10-11 12:20:44 +0800

    feat: update navigation bar layout with search input and avatar integration

--- client/src/routes/components/navigation-bar.svelte
+++ client/src/routes/components/navigation-bar.svelte
@@ -1,19 +1,20 @@
 <script lang="ts">
- import Input from "$lib/components/ui/input/input.svelte";
- import * as Avatar from "$lib/components/ui/avatar/index";
- import logo from '$lib/assets/logo.svg';
+	import Input from '$lib/components/ui/input/input.svelte';
+	import * as Avatar from '$lib/components/ui/avatar/index';
+	import { Search } from '@lucide/svelte';
 </script>
- 
 
-<div class="bg-[#6a994e]">
-
-
-<div class="max-w-3xl mx-auto p-4 flex items-center gap-4">
-  <img src="/assets/" alt="">
-<Input /> 
-<Avatar.Root>
-  <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
-  <Avatar.Fallback>CN</Avatar.Fallback>
-</Avatar.Root>
-</div>
+<div class="bg-[#B0CE88]">
+	<div class="mx-auto flex max-w-3xl items-center gap-4 p-4">
+		<div class="group flex w-full flex-row items-center rounded-md bg-white">
+			<div class="relative w-full">
+				<Search class="text-muted-background absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2" />
+				<Input type="search" placeholder="Search..." class="w-full pl-9" />
+			</div>
+		</div>
+		<Avatar.Root>
+			<Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+			<Avatar.Fallback>CN</Avatar.Fallback>
+		</Avatar.Root>
+	</div>
 </div>
