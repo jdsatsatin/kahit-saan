2d4628553eb1e123c09958873dd780f92f1b6da9
jdsatsatin <dev.jdsatsatin@gmail.com>
2025-10-11 11:53:44 +0800

    feat: implement avatar components and update navigation bar with input and avatar integration

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -2,7 +2,4 @@
   import NavigationBar from './components/navigation-bar.svelte';
 </script>
 
-
 <NavigationBar />
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
--- client/src/routes/components/navigation-bar.svelte
+++ client/src/routes/components/navigation-bar.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
- import * as NavigationMenu from "$lib/components/ui/navigation-menu/";
+ import Input from "$lib/components/ui/input/input.svelte";
+ import * as Avatar from "$lib/components/ui/avatar/index";
+ import logo from '$lib/assets/logo.svg';
 </script>
  
 
-<div>
-  <img src="" alt="">
-<NavigationMenu.Root>
- <NavigationMenu.List>
-  <NavigationMenu.Item>
-   <NavigationMenu.Link>Item One</NavigationMenu.Link>
-  </NavigationMenu.Item>
- </NavigationMenu.List>
-</NavigationMenu.Root>
+<div class="bg-[#6a994e]">
+
+
+<div class="max-w-3xl mx-auto p-4 flex items-center gap-4">
+  <img src="/assets/" alt="">
+<Input /> 
+<Avatar.Root>
+  <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+  <Avatar.Fallback>CN</Avatar.Fallback>
+</Avatar.Root>
+</div>
 </div>
