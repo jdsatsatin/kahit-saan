fb71b9fee3f81afa5662c4d40f358e5ef689bd34
jdsatsatin <dev.jdsatsatin@gmail.com>
2025-10-12 20:29:01 +0800

    feat: add kain-na-section component with store images and descriptions

---
+++ client/src/routes/components/kain-na-section.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import placeholderImage from '$lib/assets/image-placeholder.webp';
+	import placeholderImageGreen from '$lib/assets/image-placeholder-green.webp';
+	import ToyBitz from '$lib/assets/toy-bitz.jpg.png';
+	import Wcdonalds from '$lib/assets/wcdonalds.webp';
+	const stores = [
+		{
+			id: 1,
+			name: '',
+			description: 'This is the description for Store 1.',
+			image: ToyBitz
+		},
+		{
+			id: 3,
+			name: 'Sample Store 3 Cavite City',
+			description: 'This is the description for Store 3.'
+		},
+		{
+			id: 3,
+			name: 'Sample Store 4 Cavite City',
+			description: 'This is the description for Store 3.'
+		},
+		{
+			id: 3,
+			name: 'Sample Store 5 Cavite City',
+			description: 'This is the description for Store 3.'
+		},
+		{
+			id: 3,
+			name: 'Sample Store 6 Cavite City',
+			description: 'This is the description for Store 3.'
+		}
+	];
+</script>
+
+<div class="mx-auto max-w-3xl p-4">
+	<h2 class="mb-4 text-base font-bold">Nearby Stores</h2>
+	<div class="hide-scrollbar flex gap-1 overflow-x-auto pb-2">
+		{#each stores as store}
+			<div class="w-35 flex-shrink-0 rounded-lg">
+				<img
+					src={store.image || placeholderImage}
+					alt={store.name}
+					class="mb-2 aspect-square w-full rounded object-contain"
+				/>
+				<p class="text-xs font-semibold">{store.name}</p>
+			</div>
+		{/each}
+	</div>
+</div>
