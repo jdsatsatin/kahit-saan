a5a4ec1304a0f6456ae455f14083b027efef0d5c
jdsatsatin <dev.jdsatsatin@gmail.com>
2025-10-13 01:00:03 +0800

    feat: add FoodSection component to display nearby stores

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -2,8 +2,10 @@
 	import NavigationBar from './components/navigation-bar.svelte';
 	import AddressHeader from './components/address-header.svelte';
 	import StoreSection from './components/store-section.svelte';
-	import { Store } from '@lucide/svelte';
+
+	import FoodSection from './components/food-section.svelte';
 </script>
 
 <NavigationBar />
 <StoreSection />
+<FoodSection />
