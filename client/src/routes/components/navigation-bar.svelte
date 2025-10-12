ce63eec0fdd5901e5af62404a87329a82201314e
jdsatsatin <dev.jdsatsatin@gmail.com>
2025-10-12 11:49:18 +0800

    refactor: simplify search input structure in navigation bar

--- client/src/routes/components/navigation-bar.svelte
+++ client/src/routes/components/navigation-bar.svelte
@@ -6,11 +6,9 @@
 
 <div class="bg-[#B0CE88]">
 	<div class="mx-auto flex max-w-3xl items-center gap-4 p-4">
-		<div class="group flex w-full flex-row items-center rounded-md bg-white">
-			<div class="relative w-full">
-				<Search class="text-muted-background absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2" />
-				<Input type="search" placeholder="Search..." class="w-full pl-9" />
-			</div>
+		<div class="relative flex w-full flex-row items-center rounded-md bg-white">
+			<Search class="text-muted-background absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2" />
+			<Input type="search" placeholder="Search..." class="w-full pl-9" />
 		</div>
 		<Avatar.Root>
 			<Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
