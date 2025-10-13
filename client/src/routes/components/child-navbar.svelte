50564f41df4f36a97c520379143d366e4781709e
jdsatsatin <dev.jdsatsatin@gmail.com>
2025-10-13 16:20:56 +0800

    Add child-navbar component for navigation

---
+++ client/src/routes/components/child-navbar.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { ArrowLeft } from '@lucide/svelte';
+
+	export let title: string = 'Stores Near You';
+	export let backLink: string = '/';
+</script>
+
+<div class="relative sticky top-0 flex w-full max-w-6xl items-center bg-white py-2">
+	<!-- Left arrow -->
+	<a href={backLink} class="absolute left-3 flex items-center">
+		<ArrowLeft class="h-6 w-6" />
+	</a>
+
+	<!-- Center text -->
+	<p class="mx-auto text-center text-lg font-semibold">{title}</p>
+</div>
