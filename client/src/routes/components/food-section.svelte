3812e5e4bed3b85664dfd7270610ed7b43d6e58c
jdsatsatin <dev.jdsatsatin@gmail.com>
2025-10-13 20:03:49 +0800

    Refactor: adjust margin of Kain Na! header for improved layout

--- client/src/routes/components/food-section.svelte
+++ client/src/routes/components/food-section.svelte
@@ -41,7 +41,7 @@
 </script>
 
 <div class="mx-auto max-w-3xl px-4 pt-4">
-	<h2 class="mb-4 text-base font-bold">Kain Na!</h2>
+	<h2 class="mb-2 text-base font-bold">Kain Na!</h2>
 	<div class="hide-scrollbar flex gap-1 overflow-x-auto pb-2">
 		{#each products as product}
 			<div class="w-35 flex-shrink-0 rounded-lg">
