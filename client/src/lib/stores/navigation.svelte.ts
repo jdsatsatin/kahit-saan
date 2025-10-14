a6afa3271bb42321e4ee7a48bda439b43105e54c
jdsatsatin <dev.jdsatsatin@gmail.com>
2025-10-14 11:47:29 +0800

    fix: remove afterNavigate logic from navigation store

--- client/src/lib/stores/navigation.svelte.ts
+++ client/src/lib/stores/navigation.svelte.ts
@@ -1,10 +1,3 @@
 import { writable } from 'svelte/store';
-import { afterNavigate } from '$app/navigation';
 
 export let previousRoute = writable<string | null>('/');
-
-afterNavigate((from) => {
-	if (from) {
-		previousRoute.set(from.from?.url.pathname || null);
-	}
-});
