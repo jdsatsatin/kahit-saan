78ab41582b1eebb26def6156155420c87c0f51b7
jdsatsatin <dev.jdsatsatin@gmail.com>
2025-10-14 10:35:28 +0800

    fix: update backLink to use previousRoute from navigation store

--- client/src/routes/components/child-navbar.svelte
+++ client/src/routes/components/child-navbar.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import { ArrowLeft } from '@lucide/svelte';
+	import { previousRoute } from '$lib/stores/navigation.svelte';
 
 	export let title: string = 'Stores Near You';
-	export let backLink: string = '/';
 </script>
 
 <div class="relative sticky top-0 mx-auto flex w-full max-w-3xl items-center bg-white py-2">
 	<!-- Left arrow -->
-	<a href={backLink} class="absolute left-3 flex items-center">
+	<a href={$previousRoute} class="absolute left-3 flex items-center">
 		<ArrowLeft class="h-6 w-6" />
 	</a>
 
