bca2e85d6c8b27582598810f7fceea3c718fa78e
jdsatsatin <dev.jdsatsatin@gmail.com>
2025-10-14 10:35:59 +0800

    fix: remove backLink prop from ChildNavbar in stores page

--- client/src/routes/stores/[slug]/+page.svelte
+++ client/src/routes/stores/[slug]/+page.svelte
@@ -32,7 +32,7 @@
 	});
 </script>
 
-<ChildNavbar title={store?.name || 'Store'} backLink="/stores" />
+<ChildNavbar title={store?.name || 'Store'} />
 
 {#if loading}
 	<div class="mx-auto max-w-3xl px-4 py-6">
