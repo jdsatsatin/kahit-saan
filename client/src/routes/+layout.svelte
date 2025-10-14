aa60b4904d5898602dff86bbdc7d5222bfd544d6
jdsatsatin <dev.jdsatsatin@gmail.com>
2025-10-14 10:44:28 +0800

    fix: remove unused QueryClientProvider and streamline navigation handling

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
-	import { QueryClient, QueryClientProvider } from '@sveltestack/svelte-query';
 
+	import { previousRoute } from '$lib/stores/navigation.svelte';
+	import { afterNavigate } from '$app/navigation';
 	let { children } = $props();
-	const queryClient = new QueryClient();
+
+	let lastRoute: string | '/' = '/';
+
+	afterNavigate((from) => {
+		if (from) {
+			lastRoute = from.from?.url.pathname || '/';
+			previousRoute.set(lastRoute);
+		}
+	});
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<QueryClientProvider client={queryClient}>
-	{@render children?.()}
-</QueryClientProvider>
+{@render children?.()}
