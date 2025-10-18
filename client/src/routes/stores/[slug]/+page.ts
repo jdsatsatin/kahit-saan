8042a20a4a9822d03090ec905f1df615bbdb14fe
jdsatsatin <dev.jdsatsatin@gmail.com>
2025-10-18 11:31:23 +0800

    chore: remove unused store page load function and related imports

--- client/src/routes/stores/[slug]/+page.ts
+++
@@ -1,9 +0,0 @@
-import type { PageLoad } from './$types';
-import { StoreService } from '$lib/services/stores.service';
-
-export const load: PageLoad = async ({ params }) => {
-	const store = await StoreService.getStoreById(params.slug);
-	return {
-		store: store || null
-	};
-};
