9ed9eff9279db5bd6eca1abd3e8eee215ce9581c
jdsatsatin <dev.jdsatsatin@gmail.com>
2025-11-25 02:12:53 +0800

    feat: add maplibre

---
+++ client/src/routes/map/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	let mapContainer: HTMLDivElement | string;
+
+	onMount(async () => {
+		// Import only on client
+		const maplibregl = await import('maplibre-gl');
+
+		const map = new maplibregl.Map({
+			container: mapContainer,
+			style: '/styles/style-osm.json',
+			center: [120.98, 14.6], // Manila
+			zoom: 12
+		});
+
+		new maplibregl.NavigationControl();
+	});
+</script>
+
+<div bind:this={mapContainer} class="map min-h-screen"></div>
